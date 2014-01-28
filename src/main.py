948f8dbc698f868e9bf78e28863371496f18cc91
PeterRyder <peter@cloudvlab.com>
2014-01-27 23:38:46 -0500

    Initial commit of parse and analysis files

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 # main python file for MusicParser 
 # authors: Peter Ryder, Jake Lowey
 
+#### API Imports #####
+
 import music21
 import numpy
 import scipy
+
+######################
+
+#### File Imports ####
+
+import 'MusicXMLParser.py'
+import 'MusicAnalysis.py'
+
+######################
+
 
 
