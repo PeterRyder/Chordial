3febde02ccac16140c4a479172138c034c1b519d
Jacob Lowey <jeinstein63@gmail.com>
2014-02-05 20:10:50 -0500

    First HTML attempt

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>Lab 10: Databases and SQL, Intro ITWS</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <style type="text/css">
+      body{
+        background-color: green;
+      }
+      #border{
+        width: 1200px;
+        height: 900px;
+        position: relative;
+      }
+      #I {
+       background-color: red; 
+       text-align: center;
+       width: 175px;
+       height: 175px;
+       position: absolute;
+       top:475px;
+       left: 20px;
+      }
+      #II{
+       background-color: red; 
+       text-align: center;
+       width: 175px;
+       height: 175px;
+       position: absolute;
+       top:700px;
+       left: 600px;
+      }
+      #III{
+       background-color: red; 
+       text-align: center;
+       width: 175px;
+       height: 175px;
+       position: absolute;
+       top:50px;
+       left:250px;
+      }
+      #IV{
+       background-color: red; 
+       text-align: center;
+       width: 175px;
+       height: 175px;
+       position: absolute;
+       top:400px;
+       left: 650px;
+      }
+      #V{
+       background-color: red; 
+       text-align: center;
+       width: 175px;
+       height: 175px;
+       position: absolute;
+       top: 475px;
+       right:20px;
+      }
+      #VI{
+       background-color: red; 
+       text-align: center;
+       width: 175px;
+       height: 175px;
+       position: absolute;
+       top:300px;
+       left: 350px;
+      }
+      #VII{
+       background-color: red; 
+       text-align: center;
+       width: 175px;
+       height: 175px;
+       position: absolute;
+       top: 50px;
+       left: 750px;
+      }
+
+    </style>
+  </head>
+  <body>
+    <div id='border'>
+      <div id='I'><p>I</p></div>
+      <div id='II'><p>II</p></div>
+      <div id='III'><p>III</p></div>
+      <div id='IV'><p>IV</p></div>
+      <div id='V'><p>V</p></div>
+      <div id='VI'><p>VI</p></div>
+      <div id='VII'><p>VII</p></div>
+    </div>
+  </body>
+</html>
