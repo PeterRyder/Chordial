05e475e28853910a72093a65ea9661b6c433f2c7
Jacob Lowey <jeinstein63@gmail.com>
2014-02-15 16:47:37 -0500

    Set up the database to store all the collected data from the music files.  Also set up some test php and javascript in order to make sure pulling from the database was working and displaying the data was fine as well.

--- index.php
+++ index.php
@@ -1,8 +1,40 @@
+<?php
+  @ $db = new mysqli('localhost', 'root', 'chordial', 'chordial');
+
+  $dbOK = false;
+
+  if ($db->connect_error) {
+    echo '<div class="messages">Could not connect to the database. Error: ';
+    echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+  } else {
+    $dbOk = true; 
+  }
+
+  $result = $db->query("SELECT * FROM `chordial` WHERE `ID` = 1 ");
+  $row = $result->fetch_array(MYSQLI_ASSOC);
+  $title = $row["Title"];
+  $comp = $row["Composer"];
+  $prog = $row["Progression"];
+
+?>
+
+
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title>Lab 10: Databases and SQL, Intro ITWS</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <script>
+      function hello () {
+        var title = <?php echo json_encode($title);?>;
+        var composer = <?php echo json_encode($comp);?>;
+        var progression = <?php echo json_encode($prog);?>;
+        var header = title + "\nBy\n" + composer;
+        document.getElementById('title').innerHTML=header;
+      }
+    </script>
     <style type="text/css">
       body{
         background-color: green;
@@ -11,6 +43,9 @@
         width: 1600px;
         height: 900px;
         position: relative;
+      }
+      #title{
+        text-align: center;
       }
       #I {
        background-color: red; 
@@ -87,8 +122,9 @@
 
     </style>
   </head>
-  <body>
+  <body onload="hello()">
     <div id='border'>
+      <div id='title'></div>
       <div id='I'><p>I</p></div>
       <div id='II'><p>II</p></div>
       <div id='III'><p>III</p></div>
