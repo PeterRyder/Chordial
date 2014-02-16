3dd47614cb11b2a4c1aa8467188cd845b71ee7fb
Jacob Lowey <jeinstein63@gmail.com>
2014-02-15 21:19:22 -0500

    Moved CSS to a seperate file and added some potential background images.

--- index.php
+++ index.php
@@ -26,112 +26,52 @@
   <head>
     <title>Lab 10: Databases and SQL, Intro ITWS</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" href="res/stylesheet.css" type="text/css"/>
     <script>
-      function hello () {
-        var title = <?php echo json_encode($title);?>;
-        var composer = <?php echo json_encode($comp);?>;
-        var progression = <?php echo json_encode($prog);?>;
+      var i = 0;
+      var size;
+      var title;
+      var composer;
+      var progression;
+      function load () {
+        title = <?php echo json_encode($title);?>;
+        composer = <?php echo json_encode($comp);?>;
+        progression = <?php echo json_encode($prog);?>;
+        size = progression.length;
         var header = title + "\nBy\n" + composer;
         document.getElementById('title').innerHTML=header;
       }
+
+      function color(prog, i){
+          if(i < size){
+            var rn = prog[i];
+            if( i  == 0){
+              
+            } 
+            else if (i == 1){
+
+            }
+            else {
+
+            }
+          }
+      }
     </script>
     <style type="text/css">
-      body{
-        background-color: green;
-      }
-      #border{
-        width: 1600px;
-        height: 900px;
-        position: relative;
-      }
-      #title{
-        text-align: center;
-      }
-      #I {
-       background-color: red; 
-       text-align: center;
-       width: 175px;
-       height: 175px;
-       position: absolute;
-       top:475px;
-       left: 20px;
-      }
-      #II{
-       background-color: red; 
-       text-align: center;
-       width: 175px;
-       height: 175px;
-       position: absolute;
-       top: 650px;
-       left: 1000px;
-      }
-      #III{
-       background-color: red; 
-       text-align: center;
-       width: 175px;
-       height: 175px;
-       position: absolute;
-       top:50px;
-       left:450px;
-      }
-      #IV{
-       background-color: red; 
-       text-align: center;
-       width: 175px;
-       height: 175px;
-       position: absolute;
-       top:400px;
-       left: 875px;
-      }
-      #V{
-       background-color: red; 
-       text-align: center;
-       width: 175px;
-       height: 175px;
-       position: absolute;
-       top: 475px;
-       right:20px;
-      }
-      #VI{
-       background-color: red; 
-       text-align: center;
-       width: 175px;
-       height: 175px;
-       position: absolute;
-       top:275px;
-       left: 550px;
-      }
-      #VII{
-       background-color: red; 
-       text-align: center;
-       width: 175px;
-       height: 175px;
-       position: absolute;
-       top: 50px;
-       left: 1050px;
-      }
-      #G{
-       background-color: red; 
-       text-align: center;
-       width: 175px;
-       height: 175px;
-       position: absolute;
-       top: 650px;
-       left: 500px;
-      }
+      
 
     </style>
   </head>
-  <body onload="hello()">
+  <body onload="load()">
     <div id='border'>
       <div id='title'></div>
-      <div id='I'><p>I</p></div>
-      <div id='II'><p>II</p></div>
-      <div id='III'><p>III</p></div>
-      <div id='IV'><p>IV</p></div>
-      <div id='V'><p>V</p></div>
-      <div id='VI'><p>VI</p></div>
-      <div id='VII'><p>VII</p></div>
+      <div id='1'><p>I</p></div>
+      <div id='2'><p>II</p></div>
+      <div id='3'><p>III</p></div>
+      <div id='4'><p>IV</p></div>
+      <div id='5'><p>V</p></div>
+      <div id='6'><p>VI</p></div>
+      <div id='7'><p>VII</p></div>
       <div id='G'>G</div>
     </div>
   </body>
