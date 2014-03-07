4901c09e797b3c3a39fd5e682335e19763f89a3a
PeterRyder <peter@cloudvlab.com>
2014-03-06 19:19:01 -0500

    added main method to main file

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@
 #### API Imports #####
 
 from music21 import *
+import sys
 
 
 ######################
@@ -19,44 +20,48 @@ from MusicAnalysis import *
 getInfo = True
 parseMusic = True
 
-
-musicParsed = MusicXMLParser('../xml examples/BeetAnGeSample.xml')
-if getInfo:
-    musicParsed.getBasicInfo()
-
-    if musicParsed.song_title != "":
-        print "Song Title: " + musicParsed.song_title
-    if musicParsed.work_number != "":
-        print "Work Number: " + musicParsed.work_number
-    if musicParsed.work_title != "":
-        print "Work Title: " + musicParsed.work_title
-    if musicParsed.song_author != "":
-        print "Song Author: " + musicParsed.song_author
-    if musicParsed.encoding_date != "":
-        print "Encoding Date: " + musicParsed.encoding_date
-                
-    print "Software used: "     
-    for i in musicParsed.software:
-        print "  " + i
+if __name__ == "__main__":
     
+    # future passing in from front end of file location
+    s = sys.argv
     
-    print "Instruments Used:"
-    i = 0
-    for i in musicParsed.part_group:
-        print str(i) + " " + musicParsed.part_group[i]
-        i+=1
-
-    print ""
-
-# fully parse the xml
-if parseMusic:
-    musicParsed.getNotes()
-    musicParsed.sheet.show('text')
+    musicParsed = MusicXMLParser('../xml examples/BeetAnGeSample.xml')
+    if getInfo:
+        musicParsed.getBasicInfo()
+    
+        if musicParsed.song_title != "":
+            print "Song Title: " + musicParsed.song_title
+        if musicParsed.work_number != "":
+            print "Work Number: " + musicParsed.work_number
+        if musicParsed.work_title != "":
+            print "Work Title: " + musicParsed.work_title
+        if musicParsed.song_author != "":
+            print "Song Author: " + musicParsed.song_author
+        if musicParsed.encoding_date != "":
+            print "Encoding Date: " + musicParsed.encoding_date
+                    
+        print "Software used: "     
+        for i in musicParsed.software:
+            print "  " + i
         
-
-
-# analyze the music using the data structure
-
-musicAnalyze = MusicAnalysis(musicParsed)
-musicAnalyze.analyzeNotes()
+        
+        print "Instruments Used:"
+        i = 0
+        for i in musicParsed.part_group:
+            print str(i) + " " + musicParsed.part_group[i]
+            i+=1
+    
+        print ""
+    
+    # fully parse the xml
+    if parseMusic:
+        musicParsed.getNotes()
+        musicParsed.sheet.show('text')
+            
+    
+    
+    # analyze the music using the data structure
+    
+    musicAnalyze = MusicAnalysis(musicParsed)
+    musicAnalyze.analyzeNotes()
 
