1f8e836e8aa4a59c0b49a49a0c1851b0c23549db
PeterRyder1 <peter.w.ryder@gmail.com>
2014-05-05 18:47:01 -0400

    created client side GUI with working browse button and output

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'MainWindow.ui'
 #
-# Created: Fri May 02 21:04:16 2014
+# Created: Mon May 05 17:10:07 2014
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -28,7 +28,7 @@ except AttributeError:
 class Ui_mainWindow(object):
     def setupUi(self, mainWindow):
         mainWindow.setObjectName(_fromUtf8("mainWindow"))
-        mainWindow.resize(380, 333)
+        mainWindow.resize(935, 693)
         self.centralwidget = QtGui.QWidget(mainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.gridLayout = QtGui.QGridLayout(self.centralwidget)
@@ -41,9 +41,19 @@ class Ui_mainWindow(object):
         self.ChooseFile.setSizePolicy(sizePolicy)
         self.ChooseFile.setObjectName(_fromUtf8("ChooseFile"))
         self.gridLayout.addWidget(self.ChooseFile, 0, 0, 1, 1)
+        self.scrollArea = QtGui.QScrollArea(self.centralwidget)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName(_fromUtf8("scrollArea"))
+        self.scrollAreaWidgetContents = QtGui.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 834, 632))
+        self.scrollAreaWidgetContents.setObjectName(_fromUtf8("scrollAreaWidgetContents"))
+        self.gridLayout_2 = QtGui.QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.gridLayout.addWidget(self.scrollArea, 0, 1, 1, 1)
         mainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(mainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 380, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 935, 21))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         mainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(mainWindow)
@@ -57,67 +67,93 @@ class Ui_mainWindow(object):
     def retranslateUi(self, mainWindow):
         mainWindow.setWindowTitle(_translate("mainWindow", "Chordial", None))
         self.ChooseFile.setText(_translate("mainWindow", "Choose File", None))
-
-    
+        
     def update(self):
-        file_path = str(QtGui.QFileDialog.getOpenFileName())
-        print file_path
-        
-        file_path = file_path.strip('\n')
-        folder = file_path.split('/')
-        
-        file_name = folder[-1]
-        folder = folder[0:-1]
-        
-        #print file_name
-        #print folder
-        
-        folder_path = ""
-
-        
-        temp = 0
-        for i in folder:
-            if (temp != len(folder) - 1):
-                folder_path  = str(folder_path) + i + '/'
-            else:
-                folder_path = str(folder_path) + i
-            temp+=1
-        
-        print folder_path
-        
-        us = environment.UserSettings()
-        us['localCorpusPath'] = folder_path      
-        
-        b = corpus.parse(file_name)
-        b.show('text')
-        
-        bChords = b.chordify()
-        
-        #adds chordify as another voice to show better
-        for c in bChords.flat:
-            if 'Chord' not in c.classes:
-                continue
-            c.closedPosition(forceOctave=4, inPlace=True)
-        
-        #b.measures(0,2).show()
-        
-        stats = TransitionGraph()
-        
-        #adds roman numerals at the bottom
-        for c in bChords.flat.getElementsByClass('Chord'):
-            rn = roman.romanNumeralFromChord(c, key.Key('A'))
-            stats.addNumeral(rn)
-            c.addLyric(str(rn.figure))
-        #bChords.measures(0, 2).show()
-        bChords.show('text')
-        
-        
-        
-        #prints the roman numerals, i.e. lyrics
-        for c in bChords.measures(0,2).flat:
-            if 'Chord' not in c.classes:
-                continue
-            print c.lyric,
+            file_path = str(QtGui.QFileDialog.getOpenFileName())
+            print file_path
             
-        print "\n"
-        stats.printTransitions()        
+            file_path = file_path.strip('\n')
+            folder = file_path.split('/')
+            
+            file_name = folder[-1]
+            folder = folder[0:-1]
+            
+            folder_path = ""
+            
+            work_name = file_name.split('.')
+            work_name = work_name[0]
+            print "work name: " + work_name
+    
+            
+            temp = 0
+            for i in folder:
+                if (temp != len(folder) - 1):
+                    folder_path  = str(folder_path) + i + '\\'
+                else:
+                    folder_path = str(folder_path) + i
+                temp+=1
+            
+            
+            print "Analyzing " + file_name
+            print "File located here: " + folder_path       
+            print ""
+            
+            corpus.addPath(folder_path)
+            paths = corpus.getPaths()
+            for i in paths:
+                print i
+            
+            print corpus.getWork(work_name)
+            b = corpus.parse(work_name)
+            b.show('text')
+            
+            bChords = b.chordify()
+            
+            #adds chordify as another voice to show better
+            for c in bChords.flat:
+                if 'Chord' not in c.classes:
+                    continue
+                c.closedPosition(forceOctave=4, inPlace=True)
+            
+            #b.measures(0,2).show()
+            
+            stats = TransitionGraph()
+            
+            #adds roman numerals at the bottom
+            for c in bChords.flat.getElementsByClass('Chord'):
+                rn = roman.romanNumeralFromChord(c, key.Key('C'))
+                stats.addNumeral(rn)
+                c.addLyric(str(rn.figure))
+            #bChords.measures(0, 2).show()
+            #bChords.show('text')
+            
+            
+            
+            #prints the roman numerals, i.e. lyrics
+            for c in bChords.measures(0,2).flat:
+                if 'Chord' not in c.classes:
+                    continue
+                print c.lyric,
+                
+            print "\n"
+            output = stats.getTransitions()
+            
+            stats.printTransitions()
+            print ""
+            stats.printTransInOrder()
+            
+            self.label1 = QtGui.QLabel(self.scrollAreaWidgetContents)
+            self.label1.setObjectName(_fromUtf8("label1"))
+            self.gridLayout_2.addWidget(self.label1, 0, 0, 1, 1)  
+            self.label1.setText(_translate("mainWindow", stats.getTransInOrder(), None)) 
+            
+            temp = 1
+            for i in output:
+                
+                
+                self.label = QtGui.QLabel(self.scrollAreaWidgetContents)
+                self.label.setObjectName(_fromUtf8("label"))
+                self.gridLayout_2.addWidget(self.label, temp, 0, 1, 1)
+                self.label.setText(_translate("mainWindow", i[0] + "->" + i[1] + " : " + str(output[i]), None))    
+                temp += 1
+
--- src/transitions.py
+++ src/transitions.py
@@ -1,27 +1,13 @@
-'''
-class Numeral():
-    numeral = None
-    
-    def __init__(self, numeral):
-        self.numeral = numeral
-    
-    def getNumeral(self):
-        return numeral
-    
-    #ignore this for now
-    def knownTransition(self, nextNumeral):
-        return
-'''     
-        
-
 class TransitionGraph():
     #for now, ugly dictionary of dictionaries to have the pointers
     transitions = None
     prevNumeral = None
+    trans_in_order = None
     
     def __init__(self):
         self.transitions = dict()
         self.prevNumeral = None
+        self.trans_in_order = ""
    
    #add transition from previous roman numeral to the next one;
    #just adds it if it doesn't exist
@@ -30,6 +16,7 @@ class TransitionGraph():
         if s not in self.transitions:
             self.transitions[s] = 0
         self.transitions[s] += 1
+        self.trans_in_order += str(prevNum.scaleDegree) + "->" + str(nextNum.scaleDegree) + ", "
         
    #pass in class music21.roman.RomanNumeral
     def addNumeral(self, numeral1):
@@ -40,6 +27,18 @@ class TransitionGraph():
             self.prevNumeral = numeral1
             
     #prints all the transitions found
+    def getTransitions(self):
+        return self.transitions
+    
+    def getTransInOrder(self):
+        return self.trans_in_order
+    
     def printTransitions(self):
         for i in self.transitions:
             print i[0] + "->" + i[1] + " : " + str(self.transitions[i])
+            
+    def printTransInOrder(self):
+        self.trans_in_order = self.trans_in_order[0:-2]
+        print self.trans_in_order
+            
+            
